1c962ab329f1e7cdfae7fbaf06e9d5cb6de43354
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-05-24 17:46:30 +0600

    moduler pattern and product  interface and model done

---
+++ src/app/module/product/product.interface.ts
@@ -0,0 +1,24 @@
+import { Model } from "mongoose";
+
+// TypeScript types
+export type TVariant = {
+  type: string;
+  value: string;
+};
+
+export type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
+export type TProduct ={
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  tags: string[];
+  variants: TVariant[];
+  inventory: TInventory;
+};
+
+
